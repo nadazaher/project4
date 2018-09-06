478d685b236465fac1b91241937ebb2ef4476a99
NADA ZAHER <zahernada94@gmail.com>
2018-09-06 17:23:56 -0400

    favorites controller and routes work with nested resources and checked in postman

--- p4rails/app/controllers/favorites_controller.rb
+++ p4rails/app/controllers/favorites_controller.rb
@@ -1,33 +1,55 @@
 class FavoritesController < ApplicationController
 
     def index
-        render json: { favorite: Favorite.all }
-    end
+        if (params[:event_service_id])
+          @favorites = EventService.find(params[:event_service_id]).favorites
+        elsif 
+            (params[:user_id])
+          @favorites = User.find(params[:user_id]).favorites
+        else
+          @favorites = Favorite.all
+        end
+        render json: { favorites: @favorites}
+      end
 
     def show
-        id = params[:id]
-        render json: { favorite: Favorite.find(id)}
+        @favorite = Favorite.find(params[:id])
+        render json: {favorite: @favorite}
     end
 
     def create
-        @favorite = Favorite.create! favorite_params
-        render json: { favorite: @favorite }
-    end
+        if (params[:event_service_id])
+            @favorite = EventService.find(params[:event_service_id]).favorites.new(favorite_params)
 
-    def update
-        @favorite = Favorite.find(params[:id])
-        if @favorite.update! favorite_params
-            render json: { favorite: @favorite }
+        elsif (params[:user_id])
+          @favorite = User.find(params[:user_id]).favorites.new(favorite_params)
+       
         else
-            render json: { message: 'Error'}
+          @favorite = Favorite.new(favorite_params)
         end
-    end
 
-    def destroy
-        id = params[:id]
-        @favorite = Favorite.find(id).destroy
-        render json: { favorite: @favorite }
-    end
+        if @favorite.save
+          render json: { favorite: @favorite }
+        else
+          render json: { message: 'Some fields are invalid', errors: @favorite.errors }, status: :bad_request
+        end
+      end
+
+      def update
+        @favorite = Favorite.find(params[:id])
+        if @favorite.update(favorite_params)
+          render json: { favorite: @favorite}
+        else
+          render json: { message: 'Some fields are invalid', errors: @favorite.errors }, status: :bad_request
+        end
+      end
+
+      def destroy
+        @favorite = Favorite.find(params[:id])
+        @favorite.destroy
+        render json: { message: "Deleted favorite #{params[:id]}"}
+      end
+
 
     private
 
